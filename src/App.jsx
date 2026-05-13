2c370c54b9efe36fab695302dea95791b61932b4
npzulu <uthandolwamazulu@gmail.com>
2026-05-13 21:53:27 +0200

    Fix background-image class and Get Started button

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
             that breathe life into every corner of your home.
           </p>
           <button className="btn-get-started" onClick={() => navigate('products')}>
-            Explore Our Plants
+            Get Started
             <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5">
               <path d="M5 12h14M12 5l7 7-7 7"/>
             </svg>
